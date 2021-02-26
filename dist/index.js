bcdd04d4bde62b18bc6c6b7040fcc9509bf288a4
diegopatinodoprr <diegopatino@doprr.com>
2021-02-26 09:44:47 +0100

    pasagge a la version 2

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,6 @@ var pluralize = require("pluralize");
  * @Return {string}
  */
 function getPlural(str) {
-    return pluralize.plural(str) + "On est Dans la Derniere Version";
+    return pluralize.plural(str) + "On est Dans la Derniere Version 2.0.0";
 }
 exports.getPlural = getPlural;
