c9498328b7713097f1aa73c7c1cb7187d25b9318
unknown <diegopatino@doprr.com>
2021-03-12 10:52:21 +0100

    Add npm rc

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPlural = void 0;
-var pluralize = require("pluralize");
+import * as pluralize from "pluralize";
 /**
  * @Method: Returns the plural form of any noun.
  * @Param {string}
  * @Return {string}
  */
-function getPlural(str) {
-    return pluralize.plural(str) + "On est Dans la Derniere Version 2.0.1";
+export function getPlural(str) {
+    return pluralize.plural(str) + "On est Dans la Derniere Version rrrrr";
 }
-exports.getPlural = getPlural;
+//# sourceMappingURL=index.js.map
